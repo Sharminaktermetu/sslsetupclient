5729c357d9f1b970b3c1545a51a476c5124ab058
Sharminaktermetu <sharminacc1@gmail.com>
2023-11-01 19:02:33 +0600

    success

--- src/components/Productform/Productform.jsx
+++ src/components/Productform/Productform.jsx
@@ -12,7 +12,7 @@ const Productform = () => {
 
     const onSubmit = (data) => {
         data.productId = id;
-        console.log(data);
+    
         fetch('https://ssl-payment.onrender.com/ssl-request', {
             method: "POST",
             headers: { "content-type": "application/json" },
